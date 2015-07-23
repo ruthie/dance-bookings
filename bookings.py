a4bcd2b5ed8008bd18ae50d970f9bba109478442
Ruthie Byers <ruthie@sendwave.com>
2015-07-23 12:06:55 +0300

    parse out person locations and add multiple caller support
    * create __str__ methods for various objects

--- bookings.py
+++ bookings.py
@@ -1,45 +1,75 @@
 
 
 class Dance():
-    def __init__(self, date, location, band, caller):
+    def __init__(self, date, location, band, callers):
         self.date = date
         self.location = location
         self.band = band
-        self.caller = caller
+        self.callers = callers
 
-    def display(self):
-        print(self.date, self.band.name, self.caller.name)
+    def __str__(self):
+        # TODO: must be a nicer way to do this
+        callers_str = ""
+        for c in self.callers:
+            callers_str = callers_str + c.name + ", "
+        callers_str = callers_str[:-2]
+        
+        return "{self.date}, {self.location}, {self.band}, {callers_str}".format(**locals())
 
 class Band():
     def __init__(self, name, members):
         self.name = name
         self.members = members
 
+    def __str__(self):
+        s = self.name + " ("
+        # TODO: should be able to accumulate here
+        for m in self.members:
+            s = s + str(m) + ", "
+        s = s[:-2] + ")"
+        return s
+
 class Person():
     def __init__(self, name, location="Bay Area"):
         self.name = name
         self.location = location
 
+    def __str__(self):
+        return self.name
+
+def get_person_name_location(s):
+    if "[" not in s:
+        return s, "Bay Area"
+
+    (name, loc) = s.split("[")
+    return name.strip(), loc.strip()[:-1].strip()
+        
 def parse_file(filename):
     dances = []
     lines = open(filename).readlines()
 
     for l in lines:
-        (date, location, caller, band_info) = l.split(",", 3)
+        (date, dance_location, callers, band_info) = l.split(",", 3)
 
-        print(band_info)
+        callers_list = []
+        for c in callers.split("and"):
+            (caller_name, caller_location) = get_person_name_location(c)
+            callers_list.append(Person(caller_name, caller_location))
+
         (band_name, band_members) = band_info.split("(")
-        band_members_list = band_members[:-1].split(",")
-        # TODO: band and caller locations
+        band_members_list = []
+        for m in band_members.strip()[:-1].split(","):
+            (name, location) = get_person_name_location(m)
+            band_members_list.append(Person(name=name, location=location))
         
         dances.append(Dance(
             date = date.strip(), # TODO: make an actual datetime
-            location = location.strip(),
+            location = dance_location.strip(),
             band = Band(
                 name = band_name.strip(),
-                members = [Person(n.strip()) for n in band_members_list],
+                members = band_members_list
             ),
-            caller = Person(caller.strip())
+            callers = callers_list
         ))
     
     return dances
@@ -48,5 +78,5 @@ if __name__ == "__main__":
     filename = '/Users/ruthie/Desktop/contra_bookings/bacds_bookings.csv'
     dances = parse_file(filename)
     for d in dances:
-        d.display()
+        print(d)
         
