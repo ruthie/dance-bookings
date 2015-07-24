0e3ca2e35de56edeec95bdd1d52c2fb064dfa7ea
Ruthie Byers <ruthie@sendwave.com>
2015-07-24 15:58:43 +0200

    infer start and end dates from dance  list

--- dance_vizualizations.py
+++ dance_vizualizations.py
@@ -3,15 +3,15 @@ from bookings import parse_file
 from viz import write_html, write_css
 
 def write_dance_visualizations():
-    # TODO: take start and end dates from list of dances
-    start = date(year=2013, month=10, day=1)
-    end = date(year=2015, month=9, day=30)
-    write_html(start, end, '/Users/ruthie/Desktop/contra_bookings/viz.html')
-
-    filename = '/Users/ruthie/Desktop/contra_bookings/bacds_bookings.csv'
-    dances = parse_file(filename)
+    input_filename = '/Users/ruthie/Desktop/contra_bookings/bacds_bookings.csv'
+    html_filename = '/Users/ruthie/Desktop/contra_bookings/viz.html'
+    css_filename = '/Users/ruthie/Desktop/contra_bookings/viz.css'
     
-    write_css(dances, '/Users/ruthie/Desktop/contra_bookings/viz.css')
+    dances = parse_file(input_filename)
+    dance_dates = [d.date for d in dances]
+
+    write_html(min(dance_dates), max(dance_dates), html_filename)
+    write_css(dances, css_filename)
     
 if __name__ == "__main__":
     write_dance_visualizations()
