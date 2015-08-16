d3680b53db8389f1dc383704fbfc24600c569bdd
Ruthie Byers <ruthie@sendwave.com>
2015-08-16 16:51:06 -0700

    way to get top locations per band
    * added booker information
    * used Counters instead of hand rolling equivalent logic yay

--- bookings.py
+++ bookings.py
@@ -1,5 +1,19 @@
 from datetime import date
-from collections import OrderedDict
+from collections import OrderedDict, Counter
+
+BOOKERS_FOR_LOCATIONS = {
+    'Berkeley': 'Erik Hoffman',
+    'Hayward': 'Kelsey Hartman',
+    'SF': 'Chris Knepper',
+    'Palo Alto': 'Alan Winston',
+    'Queer Contra': 'Margaret Pigman',
+    'Circle Left': 'Margaret Pigman',
+    'San Jose': 'Shirley Worth',
+    'San Rafael': 'Susan Amato',
+    'Santa Rosa': 'Michael Martin',
+    'Petaluma': 'Craig Meltzner',
+    'Sebastapol': 'Andy Westbom'
+}
 
 class Dance():
     def __init__(self, date, location, band, callers):
@@ -35,6 +49,17 @@ class Person():
     def __str__(self):
         return self.name + "/" + self.location
 
+class Location():
+    def __init__(self, name, booker):
+        self.name = name
+        self.booker = booker
+
+def get_booker_for_location(l):
+    if l in BOOKERS_FOR_LOCATIONS:
+        return BOOKERS_FOR_LOCATIONS[l]
+
+    return "Unknown"
+        
 def get_name_location(s):
     if "[" not in s:
         return s.strip(), None
@@ -74,10 +99,15 @@ def parse_file(filename):
                 member_location = band_location
             band_members_list.append(Person(name=member_name, location=member_location))
 
+        dance_location = dance_location.strip()
+            
         # actually create the band
         dances.append(Dance(
             date = date_from_string(dance_date),
-            location = dance_location.strip(),
+            location = Location(
+                name = dance_location,
+                booker = get_booker_for_location(dance_location)
+            ),
             band = Band(
                 name = band_name.strip(),
                 members = band_members_list
@@ -86,7 +116,7 @@ def parse_file(filename):
         ))
     
     return dances
-
+                      
 def get_most_booked_musicians(dances):
     return frequency_dict(dances, lambda x: x.band.members)
 
@@ -140,4 +170,9 @@ if __name__ == "__main__":
     callers = get_most_booked_callers(dances)
     musicians = get_most_booked_musicians(dances)
 
-    print_dict_value_ordered(musicians)
+    most_booked_bands = Counter(bands).most_common(12)
+    for (band_name, _) in most_booked_bands:
+        gigs = get_gigs_for_band(band_name, dances)
+        gig_counts = frequency_dict(gigs, lambda x: [x.location])
+        print(band_name)
+        print(gig_counts)
